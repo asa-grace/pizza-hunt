c32c79e432b2306f861bde3b6d13c7954bcbdfad
Asa Grace <asag182@gmail.com>
2021-08-19 14:27:21 -0600

    add comment

--- controllers/pizza-controller.js
+++ controllers/pizza-controller.js
@@ -24,18 +24,11 @@ const pizzaController = {
                 path: 'comments',
                 select: '-__v'
             })
-            .select('__v')
-            .then(dbPizzaData => {
-                // If no pizza is found, send 404
-                if (!dbPizzaData) {
-                    res.status(404).json({ message: 'No pizza found with this id!' });
-                    return;
-                }
-                res.json(dbPizzaData);
-            })
+            .select('-__v')
+            .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => {
                 console.log(err);
-                res.status(400).json(err);
+                res.status(400);
             });
     },
 
@@ -43,7 +36,7 @@ const pizzaController = {
     createPizza({ body }, res) {
         Pizza.create(body)
             .then(dbPizzaData => res.json(dbPizzaData))
-            .catch(err => res.status(400).json(err));
+            .catch(err => res.json(err));
     },
 
     // update pizza by id
@@ -56,7 +49,7 @@ const pizzaController = {
                 }
                 res.json(dbPizzaData);
             })
-            .catch(err => res.status(404).json(err));
+            .catch(err => res.json(err));
     },
 
     // delete pizza
@@ -69,7 +62,7 @@ const pizzaController = {
                 }
                 res.json(dbPizzaData);
             })
-            .catch(err => res.status(400).json(err));
+            .catch(err => res.json(err));
     }
 };
 
